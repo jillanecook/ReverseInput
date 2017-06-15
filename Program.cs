a6c7c70721b34ab99e93b6ba948697bd462edce8
jillanecook <cookjillane@gmail.com>
2017-06-15 15:56:57 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a Number");
+            int Number = int.Parse(Console.ReadLine());
+            int Reverse = 0;
+            while (Number > 0)
+            {
+                int Remainder = Number % 10;
+                Reverse = (Reverse * 10) + Remainder;
+                Number = Number / 10;
+
+            }
+            Console.WriteLine("Reverse Number:" + Reverse);
+            Console.ReadLine();
+        }
+    }
+}
